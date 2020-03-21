2b0bb9513039b104c1551a3ff713cd98f084627e
Dan Foley <dan@dan-foley.com>
2020-03-21 21:05:58 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -15,13 +15,29 @@ class Student
   end
   
   def save
+    if self.id
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO students (name, grade) VALUES
+        (?, ?)
+      SQL
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    end
+  end
+  
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
+  def update
     sql = <<-SQL
-      INSERT INTO students (name, grade) VALUES
-      (?, ?)
+      UPDATE students SET name = ?, grade = ? WHERE id = ?
     SQL
-    DB[:conn].execute(sql, self.name, self.grade)
-    binding.pry
-    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+    DB[:conn].execute(sql, self.name, self.grade, self.id)
   end
   
   def self.create_table
